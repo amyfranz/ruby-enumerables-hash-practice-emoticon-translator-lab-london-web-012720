b904f12136ffdc37465f420dca8f8076c897329b
Amy Franz <amy.franz@gmail.com>
2020-01-10 22:18:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,7 @@ def load_library (file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  library = YAML.load_file(file_path)
-  return library
+  library = load_library(file_path)
 end
 
 def get_english_meaning
