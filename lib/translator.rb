29f9f3e8a2e285f2547909c252ec3047e85faec3
Amy Franz <amy.franz@gmail.com>
2020-01-10 21:53:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,16 @@
 require "yaml"
 
-def load_library
-  library = YAML.load_file("./lib/emoticons.yml")
-  library
+def load_library (file_path)
+  library = YAML.load_file(file_path)
+  new_hash = {:get_meaning => {}, :get_emoticon =>{}}
+  library.each_key do |meaning|
+    puts library[meaning][1]
+    puts "cool"
+    puts new_hash
+    new_hash[:get_meaning][meaning] = library[meaning][1]
+    #new_hash[:get_emoticon] >> library[meaning]
+  end
+  return new_hash
 end
 
 def get_japanese_emoticon
